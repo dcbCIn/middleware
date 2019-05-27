b8071d576d4150e61997b532ca0efd1303626a2c
Daniel Brandão <dcb@cin.ufpe.br>
2019-05-27 19:27:52 -0300

    Ajustado para que o programa não trate um erro como resultado Empate

--- lib/dist/jankenpoProxy.go
+++ lib/dist/jankenpoProxy.go
@@ -17,7 +17,7 @@ func (jp JankenpoProxy) Play(player1Move, player2Move string) (float64, error) {
 	inv := *NewInvocation(jp.ObjectId, jp.Host, jp.Port, lib.FunctionName(), []interface{}{player1Move, player2Move})
 	termination, err := jp.requestor.Invoke(inv)
 	if err != nil {
-		return 0, err
+		return -1, err
 	}
 	winner := termination.Result.(float64)
 	return winner, nil
